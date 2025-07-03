6dd0dd3c6d314d01e860c0c1a0d53fbc9ce58e87
kumar <kumar@DESKTOP-VQ8FSOP>
2025-07-03 11:19:58 +0530

    Initial Commit3

---
+++ TNSIF_Java_programs/src/packagesdemo/PackageMain.java
@@ -0,0 +1,14 @@
+package packagesdemo;
+
+import packagesandaccessmodifiers.PackageExample;
+
+public class PackageMain {
+
+	public static void main(String[] args) {
+
+		PackageExample pe = new PackageExample();
+		pe.display();
+
+	}
+
+}
